e7cd566c0969fa494e3dc522add01ec00c8f2319
alip.putra <alip.putra@schlix.com>
2020-02-05 20:12:47 +0700

    handle empty innerhtml

--- tradingview/macros/tradingview/tradingview.js
+++ tradingview/macros/tradingview/tradingview.js
@@ -57,55 +57,58 @@ function tradingview_render()
     }
     var processTradingText = function ()
     {
-        var text = $('[itemprop="articleSection"]')[0].innerHTML;
-        var insert_macros = getTradingAttributesFromText(text, 'inserttradingview');
-        if (insert_macros) {
-            for (var i = 0; i < insert_macros.length; i++) {
-                var attrs = insert_macros[i].attributes;
-                var macro = insert_macros[i].macro;
-                var theme = (attrs.theme !== undefined) ? attrs.theme : 'light';
-                var type_attr = (attrs.type !== undefined) ? attrs.type : 'candle';
+        var element = $('[itemprop="articleSection"]')[0];
+        if(element){
+            var text = element.innerHTML;
+            var insert_macros = getTradingAttributesFromText(text, 'inserttradingview');
+            if (insert_macros) {
+                for (var i = 0; i < insert_macros.length; i++) {
+                    var attrs = insert_macros[i].attributes;
+                    var macro = insert_macros[i].macro;
+                    var theme = (attrs.theme !== undefined) ? attrs.theme : 'light';
+                    var type_attr = (attrs.type !== undefined) ? attrs.type : 'candle';
 
-                var ty = {bar: 0, candle: 1, line: 2, area: 3, renko: 4, kagi: 5};
-                var type = ty[type_attr];
+                    var ty = {bar: 0, candle: 1, line: 2, area: 3, renko: 4, kagi: 5};
+                    var type = ty[type_attr];
 
-                var tm = {daily: 'D', weekly: 'W', monthly: 'M'};
-                var timeframe = attrs.timeframe !== undefined ? tm[attrs.timeframe] : 'D';
+                    var tm = {daily: 'D', weekly: 'W', monthly: 'M'};
+                    var timeframe = attrs.timeframe !== undefined ? tm[attrs.timeframe] : 'D';
 
-                if (attrs.symbol != undefined)
-                {
-                    var url = 'https://s.tradingview.com/widgetembed/?symbol=' + attrs.symbol + '&interval=' + timeframe + '&theme=' + theme + '&style=' + type;
-                    var embed = '<iframe height="500" style="width: 100%; margin: 0 !important; padding: 0 !important;" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen="" src="' + encodeURI(url) + '"></iframe>';
-                    text = text.replace(macro, embed);
+                    if (attrs.symbol != undefined)
+                    {
+                        var url = 'https://s.tradingview.com/widgetembed/?symbol=' + attrs.symbol + '&interval=' + timeframe + '&theme=' + theme + '&style=' + type;
+                        var embed = '<iframe height="500" style="width: 100%; margin: 0 !important; padding: 0 !important;" frameborder="0" allowtransparency="true" scrolling="no" allowfullscreen="" src="' + encodeURI(url) + '"></iframe>';
+                        text = text.replace(macro, embed);
+                    }
                 }
             }
-        }
-        //hover
-        var hover_macros = getTradingAttributesFromText(text, 'tradingviewhover');
+            //hover
+            var hover_macros = getTradingAttributesFromText(text, 'tradingviewhover');
 
-        if (hover_macros) {
-            for (var i = 0; i < hover_macros.length; i++) {
-                var attrs = hover_macros[i].attributes;
-                var macro = hover_macros[i].macro;
-                var theme = (attrs.theme !== undefined) ? attrs.theme : 'light';
-                var type_attr = (attrs.type !== undefined) ? attrs.type : 'candle';
+            if (hover_macros) {
+                for (var i = 0; i < hover_macros.length; i++) {
+                    var attrs = hover_macros[i].attributes;
+                    var macro = hover_macros[i].macro;
+                    var theme = (attrs.theme !== undefined) ? attrs.theme : 'light';
+                    var type_attr = (attrs.type !== undefined) ? attrs.type : 'candle';
 
-                var ty = {bar: 0, candle: 1, line: 2, area: 3, renko: 4, kagi: 5};
-                var type = ty[type_attr];
+                    var ty = {bar: 0, candle: 1, line: 2, area: 3, renko: 4, kagi: 5};
+                    var type = ty[type_attr];
 
-                var tm = {daily: 'D', weekly: 'W', monthly: 'M'};
-                var timeframe = attrs.timeframe !== undefined ? tm[attrs.timeframe] : 'D';
+                    var tm = {daily: 'D', weekly: 'W', monthly: 'M'};
+                    var timeframe = attrs.timeframe !== undefined ? tm[attrs.timeframe] : 'D';
 
-                if (attrs.symbol != undefined)
-                {
-                    var embed = '<iframe height=500 width=500 style=margin:0!important;padding:0!important; frameborder=0 allowtransparency=true scrolling=no allowfullscreen src=https://s.tradingview.com/widgetembed/?symbol=' + attrs.symbol + '&interval=' + timeframe.toUpperCase() + '&theme=' + theme + '&style=' + type + '></iframe>';
-                    var tag = '<a href="#" id="tradingview-link" title="' + embed + '" rel="tradingview-tooltip" style="white-space: nowrap;">' + attrs.symbol + '</a>';
+                    if (attrs.symbol != undefined)
+                    {
+                        var embed = '<iframe height=500 width=500 style=margin:0!important;padding:0!important; frameborder=0 allowtransparency=true scrolling=no allowfullscreen src=https://s.tradingview.com/widgetembed/?symbol=' + attrs.symbol + '&interval=' + timeframe.toUpperCase() + '&theme=' + theme + '&style=' + type + '></iframe>';
+                        var tag = '<a href="#" id="tradingview-link" title="' + embed + '" rel="tradingview-tooltip" style="white-space: nowrap;">' + attrs.symbol + '</a>';
 
-                    text = text.replace(macro, tag);
+                        text = text.replace(macro, tag);
+                    }
                 }
             }
+            element.innerHTML = text;
         }
-        $('[itemprop="articleSection"]')[0].innerHTML = text;
     }
     processTradingText();
 }
